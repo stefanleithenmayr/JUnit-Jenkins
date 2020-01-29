af021688b9bd0ccdf3f4cadb672adcebf12150aa
[stefanleithenmayr] <s.leithenmayr@gmx.at>
2020-01-29 22:45:38 +0100

    project added

---
+++ jenkinsdemo/src/test/java/tests.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class tests {
+
+    @Test
+    void t01_test(){
+        assertThat("test").isEqualTo("test1");
+    }
+}
